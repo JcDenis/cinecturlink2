b42aadbbb9c0e8c07f387380c376d37932052d1b
Jean-Christian Denis <contact@jcdenis.fr>
2021-09-08 01:49:50 +0200

    rewrite admin page. first part. categories ok

--- js/cinecturlink2.js
+++ js/cinecturlink2.js
@@ -1,102 +1,31 @@
-/* -- BEGIN LICENSE BLOCK ----------------------------------
- * This file is part of cinecturlink2, a plugin for Dotclear 2.
- * 
- * Copyright (c) 2009-2010 JC Denis and contributors
- * jcdenis@gdwd.com
- * 
- * Licensed under the GPL version 2.0 license.
- * A copy of this license is available in LICENSE file or at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * -- END LICENSE BLOCK ------------------------------------*/
+/*global $, dotclear */
+'use strict';
 
-;if(window.jQuery) (function($){
-	$.fn.openGoogle = function(lang,target){
-		return this.each(function(){
-			$(this).click(function(){
-				var val = $(target).attr('value');
-				if (val!=''){
-					searchwindow=window.open('http://www.google.com/search?hl='+lang+'&q='+val,'search','scrollbars=yes,width=800,height=600,resize=yes,toolbar=yes,menubar=yes');
-					searchwindow.focus();
-				}
-				return false;
-			});
-		});
-	}
-	$.fn.openAmazon = function(lang,target){
-		return this.each(function(){
-			$(this).click(function(){
-				var val = $(target).attr('value');
-				if (val!=''){
-					searchwindow=window.open('http://www.amazon.fr/exec/obidos/external-search?keyword='+val+'&mode=blended','search','scrollbars=yes,width=800,height=600,resize=yes,toolbar=yes,menubar=yes');
-					searchwindow.focus();
-				}
-				return false;
-			});
-		});
-	}
-	$.fn.fillLink = function(target){
-		return this.each(function(){
-			$(this).change(function(){
-				$(target).attr('value',$(this).attr('value'));
-				return false;
-			});
-		});
-	}
-})(jQuery);
-
-var dragsort = ToolMan.dragsort();
-$(function(){
- dragsort.makeTableSortable($('#links-list-cat').get(0),
- dotclear.sortable.setHandle,dotclear.sortable.saveOrder);
-});
-dotclear.sortable = {
-  setHandle: function(item) {
-	var handle = $(item).find('td.handle').get(0);
-	while (handle.firstChild) {
-		handle.removeChild(handle.firstChild);
-	}
-	item.toolManDragGroup.setHandle(handle);
-	handle.className = handle.className+' handler';
-  },
-  saveOrder: function(item) {
-	var group = item.toolManDragGroup;
-	var order = document.getElementById('cats_order');
-	group.register('dragend', function() {
-		order.value = '';
-		items = item.parentNode.getElementsByTagName('tr');
-		for (var i=0; i<items.length; i++) {
-			order.value += items[i].id.substr(2)+',';
-		}
-	});
-  }
-};
-$(function() {
-
-	$filtersform = $('#filters-form');
-	$filtersform.before('<p><a id="filter-control" class="form-control" href="plugin.php?p=cinecturlink2" style="display:inline">'+dotclear.msg.filter_posts_list+'</a></p>')
-
-	if( dotclear.msg.show_filters == 'false' ) {
-		$filtersform.hide();
-	} else {
-		$('#filter-control')
-			.addClass('open')
-			.text(dotclear.msg.cancel_the_filter);
-	}
-
-	$('#filter-control').click(function() {
-		if( $(this).hasClass('open') ) {
-			if( dotclear.msg.show_filters == 'true' ) {
-				return true;
-			} else {
-				$filtersform.hide();
-				$(this).removeClass('open')
-					   .text(dotclear.msg.filter_posts_list);
-			}
-		} else {
-			$filtersform.show();
-			$(this).addClass('open')
-				   .text(dotclear.msg.cancel_the_filter);
-		}
-		return false;
-	});
+$(function () {
+  $('#c2itemslist').sortable({
+    cursor: 'move',
+  });
+  $('#c2itemslist tr')
+    .on('mouseenter', function () {
+      $(this).css({
+        cursor: 'move',
+      });
+    })
+    .on('mouseleave', function () {
+      $(this).css({
+        cursor: 'auto',
+      });
+    });
+  $('#c2items').on('submit', function () {
+    let order = [];
+    $('#c2itemslist tr td input.position').each(function () {
+      order.push(this.name.replace(/^order\[([^\]]+)\]$/, '$1'));
+    });
+    $('input[name=im_order]')[0].value = order.join(',');
+    return true;
+  });
+  $('#c2itemslist tr td input.position').hide();
+  $('#c2itemslist tr td.handle').addClass('handler');
+  dotclear.condSubmit('#c2items tr td input[name^=items_selected]', '#c2items #remove-action');
+  dotclear.responsiveCellHeaders(document.querySelector('#c2items table'), '#c2items table', 2);
 });
