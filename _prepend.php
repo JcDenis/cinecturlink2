956d48b4f45e5e7ff26188edf887acec5e5ad085
Jean-Christian Denis <contact@jcdenis.fr>
2021-09-07 15:05:53 +0200

    fix prepend: cleanup, update license, move activityReport behavior

--- _prepend.php
+++ _prepend.php
@@ -1,46 +1,43 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-#
-# This file is part of cinecturlink2, a plugin for Dotclear 2.
-# 
-# Copyright (c) 2009-2013 Jean-Christian Denis and contributors
-# contact@jcdenis.fr http://jcd.lv
-# 
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-#
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief cinecturlink2, a plugin for Dotclear 2
+ * 
+ * @package Dotclear
+ * @subpackage Plugin
+ * 
+ * @author Jean-Christian Denis and Contributors
+ * 
+ * @copyright Jean-Christian Denis
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
 if (!defined('DC_RC_PATH')) {
-
-	return null;
+    return null;
 }
 
-$d = dirname(__FILE__).'/inc/';
+$d = dirname(__FILE__) . '/inc/';
 
-$__autoload['cinecturlink2']			= $d.'class.cinecturlink2.php';
-$__autoload['cinecturlink2Context']	= $d.'lib.cinecturlink2.context.php';
-$__autoload['sitemapsCinecturlink2']	= $d.'lib.sitemaps.cinecturlink2.php';
+$__autoload['cinecturlink2'] = $d . 'class.cinecturlink2.php';
+$__autoload['cinecturlink2Context']    = $d . 'lib.cinecturlink2.context.php';
+$__autoload['sitemapsCinecturlink2']    = $d . 'lib.sitemaps.cinecturlink2.php';
+$__autoload['cinecturlink2ActivityReportBehaviors']    = $d . 'lib.cinecturlink2.activityreport.php';
 
 $core->url->register(
-	'cinecturlink2',
-	'cinecturlink',
-	'^cinecturlink(?:/(.+))?$',
-	array('urlCinecturlink2', 'c2Page')
+    'cinecturlink2',
+    'cinecturlink',
+    '^cinecturlink(?:/(.+))?$',
+    ['urlCinecturlink2', 'c2Page']
 );
 
-# Add cinecturlink2 report on plugin activityReport
+$core->addBehavior(
+    'sitemapsDefineParts',
+    ['sitemapsCinecturlink2', 'sitemapsDefineParts']
+);
+$core->addBehavior(
+    'sitemapsURLsCollect',
+    ['sitemapsCinecturlink2', 'sitemapsURLsCollect']
+);
+
 if (defined('ACTIVITY_REPORT')) {
-	require_once $d.'lib.cinecturlink2.activityreport.php';
+    cinecturlink2ActivityReportBehaviors::add($core);
 }
-
-# cinecturlink2 libraries for sitemaps
-$core->addBehavior(
-	'sitemapsDefineParts',
-	array('sitemapsCinecturlink2', 'sitemapsDefineParts')
-);
-$core->addBehavior(
-	'sitemapsURLsCollect',
-	array('sitemapsCinecturlink2', 'sitemapsURLsCollect')
-);
